70944dbebcc0e433376c3adf1404df07de6b36fb
benoitadam <b.adam.info@gmail.com>
2023-06-27 23:35:56 +0200

    test store object

--- test/stored.spec.ts
+++ test/stored.spec.ts
@@ -1,10 +1,14 @@
 import { getStored, setStored } from '../src/stored';
 
 describe('stored', () => {
-
-  test(`test1`, () => {
+  test(`store string`, () => {
     setStored('test1', 'abc');
     expect(getStored('test1')).toEqual('abc');
+  });
+  
+  test(`store object`, () => {
+    setStored('test2', { a: 1 });
+    expect(getStored('test2')).toEqual({ a: 1 });
   });
 });
 

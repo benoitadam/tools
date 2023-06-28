80da62eb23f4e5eae7a5d94dca143bd75a238258
vegone-xr <vegone@protonmail.com>
2023-06-28 16:39:25 +0200

    toArray type

--- src/cast/toArray.ts
+++ src/cast/toArray.ts
@@ -1,5 +1,4 @@
-export function toArray<T extends string|number|Date|{}>(v: T): T[];
-export function toArray<T = any>(v: T[]): T[];
+export function toArray<T = any>(v: T[]|T|null|undefined): T[];
 export function toArray<T = any>(v: any, def: T[]): T[];
 export function toArray<T = any>(v: any, def: T[] = []): T[] {
     return v === null || v === undefined ? def : Array.isArray(v) ? v : [v];
